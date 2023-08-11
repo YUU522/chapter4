6ec1da1ebe1e2d584c2223c023c6ce26b7982dfc
YUU522 <kuromu2750@gmail.com>
2023-08-11 07:44:38 +0000

    [pagenation]4-22

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about'=>'homes#about' ,as:'about'
-  resources :post_images, only: [:new, :index, :show, :create,:destory]do
+  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
       resource :favorites, only: [:create, :destroy]
       resources :post_comments,only:[:create,:destroy]
   end
